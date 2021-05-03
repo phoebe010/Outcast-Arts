4c889bef33a437bd8b9a6f85bece36f3ebb1dce0
phuong0 <phuongunguyen4@gmail.com>
2021-05-03 11:46:19 -0700

    camera follow and walls
    started on trying to spawn trash but didn't finish

---
+++ Outcast Arts/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float followDistance;
+    public Vector3 followOffset;
+    public float cameraFollowSpeed = 0.1f;
+    public float cameraRotationSpeed = 1.0f;
+
+    Vector3 CalculateCameraTargetPosition()
+    {
+        Vector3 targetPosition = target.transform.position;
+
+        Vector3 followVector = transform.forward * -followDistance;
+
+        Vector3 cameraTargetPosition = targetPosition + followVector + followOffset;
+
+        return cameraTargetPosition;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = CalculateCameraTargetPosition();
+
+        transform.rotation = Quaternion.LookRotation(target.transform.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraTargetPosition = CalculateCameraTargetPosition();
+
+        transform.position = Vector3.Lerp(transform.position, cameraTargetPosition,
+            cameraFollowSpeed * Time.deltaTime);
+
+        //Which way is the capsule facing - target.transform.forward
+        Vector3 targetForward = target.transform.forward;
+
+        //Taking the camera direction and slowly rotating towards the capsule forward
+        Vector3 direction = Vector3.RotateTowards(transform.forward, targetForward,
+            cameraRotationSpeed * Time.deltaTime, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
---
+++ Outcast Arts/Assets/Scripts/TrashSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject trashPrefab;
+    public float respawnTime = 1.2f;
+
+    private GameObject trashObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        if (trashObject == null)
+        {
+            trashObject = GameObject.Instantiate(trashPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+}
